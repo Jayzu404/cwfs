0e2a0db19ebc9727d38dab58e72e50487d3a2bf1
teejay <tjayarancinaofficial@gmail.com>
2025-08-09 03:18:23 +0800

    session 0

---
+++ Session0.java
@@ -0,0 +1,38 @@
+public class Session0 {
+    public static void main(String[] args) {
+
+        //Task: Print the pattern below
+        //123
+        //456
+        //7 9
+
+        //Teejay's solution
+        // int n = 1;
+        // for(int r = 1; r <= 3; r++){
+        //     for(int c = 1;c <= 3; c++){
+        //         if(r == 3 && c == 2){
+        //             System.out.print(" ");
+        //             n++;
+        //         }
+        //         else {
+        //             System.out.print(n++);
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+        //(Shiel,Ladylyn,Ritchie)'s solution
+        // for(int r = 1; r <= 3; r++){
+        //     for(int c = 1;c <= 3; c++){
+        //         if(r == 3 && c == 2){
+        //             System.out.print(" ");
+        //         }
+        //         else {
+        //             int num = (r*3) - 3 + c;
+        //             System.out.print(num);
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+    }
+}
